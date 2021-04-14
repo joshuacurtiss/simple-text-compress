3140616bdc852076f51c29b3de83a786274af375
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-04-13 21:25:28 -0500

    Test compression/decompression of JSON.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-console */
 const https = require('https');
-const { compress, decompress } = require('../src');
+const {
+    compress, compressJson, decompress, decompressJson,
+} = require('../src');
 
 // We will use some random Lorem Ipsum text for the test.
 const url = 'https://loripsum.net/api/50/long/prude/plaintext';
 
 const toKb = (bytes) => Math.floor(bytes / 102.4) / 10;
 const toPct = (a, b) => Math.floor(a * 1000 / b) / 10; // eslint-disable-line no-mixed-operators
+const report = (orig, result) => {
+    const resultLen = JSON.stringify(result).length;
+    console.log(
+        'Original size:', toKb(orig.length), 'kb',
+        '\nDictionary size:', Object.keys(result.dict).length,
+        '\nCompressed size:', toKb(result.data.length), 'kb', `(${toPct(result.data.length, orig.length)}%)`,
+        '\nFull packet size:', toKb(resultLen), 'kb', `(${toPct(resultLen, orig.length)}%)`, '(includes dictionary and JSON encoding)',
+    );
+};
 
 https.get(url, (res) => {
     res.setEncoding('utf8');
@@ -15,16 +26,25 @@ https.get(url, (res) => {
         body += data;
     });
     res.on('end', () => {
-        const result = compress(body);
-        const resultLen = JSON.stringify(result).length;
-        console.log(
-            'Original size:', toKb(body.length), 'kb',
-            '\nDictionary size:', Object.keys(result.dict).length,
-            '\nCompressed size:', toKb(result.data.length), 'kb', `(${toPct(result.data.length, body.length)}%)`,
-            '\nFull packet size:', toKb(resultLen), 'kb', `(${toPct(resultLen, body.length)}%)`, '(includes dictionary and JSON encoding)',
-        );
-        const decompResult = decompress(result.data, result.dict);
-        if (decompResult === body) console.log('Decompresses ok!');
-        else console.log('Failed to decompress.');
+        // Test compressing a standard blob of text
+        console.log('\u001b[1;31mTest Plain Text', '\u001b[0m');
+        let result = compress(body);
+        let decompResult = decompress(result.data, result.dict);
+        report(body, result);
+        console.log('Decompress:', decompResult === body ? 'Success!' : 'Failed.');
+        // Test compressing JSON
+        console.log('\u001b[1;31mTest JSON object', '\u001b[0m');
+        const data = body.split('\n').map((text, index) => ({
+            index,
+            text,
+            length: text.length,
+            first: text.split(/\b/)[0],
+        })).filter((obj) => obj.length);
+        result = compressJson(data);
+        decompResult = decompressJson(result.data, result.dict);
+        const dataString = JSON.stringify(data);
+        const decompResultString = JSON.stringify(decompResult);
+        report(dataString, result);
+        console.log('Decompress:', decompResultString === dataString ? 'Success!' : 'Failed.');
     });
 });
